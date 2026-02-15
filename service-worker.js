3af5efa27d7ac2347e9cd7b6da896d469f45012f
KnightOfCyber <durgasatish27@gmail.com>
2026-02-15 19:02:28 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "ludo8-cache-v3";
+const CACHE_NAME = "ludo8pwa-v1";
 
-const FILES_TO_CACHE = [
+const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
@@ -10,22 +10,21 @@ const FILES_TO_CACHE = [
   "./icon-192.png",
   "./icon-512.png",
 
-  // sounds
   "./sounds/dice.mp3",
   "./sounds/move.mp3",
-  "./sounds/capture.wav",
-  "./sounds/music.mp3"
+  "./sounds/music.mp3",
+  "./sounds/capture.wav"
 ];
 
-// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
-// ACTIVATE
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -39,24 +38,10 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
-// FETCH (offline support)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      return (
-        cachedResponse ||
-        fetch(event.request).then((networkResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        })
-      ).catch(() => {
-        // If offline and request not found in cache
-        if (event.request.mode === "navigate") {
-          return caches.match("./index.html");
-        }
-      });
+      return cachedResponse || fetch(event.request);
     })
   );
 });
