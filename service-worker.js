b680a816216748f5a6fe4368bb282cab7827e43b
KnightOfCyber <durgasatish27@gmail.com>
2026-02-15 19:11:07 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ludo8pwa-v1";
+const CACHE_NAME = "ludo8-cache-v3";
 
 const ASSETS = [
   "./",
@@ -8,40 +8,45 @@ const ASSETS = [
   "./manifest.json",
   "./Board.png",
   "./icon-192.png",
-  "./icon-512.png",
-
-  "./sounds/dice.mp3",
-  "./sounds/move.mp3",
-  "./sounds/music.mp3",
-  "./sounds/capture.wav"
+  "./icon-512.png"
 ];
 
+// install
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => {
+          if (event.request.mode === "navigate") {
+            return caches.match("./index.html");
+          }
+        });
     })
   );
 });
